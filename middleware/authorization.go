90569bfbc6cf93e0bebec733a074b05f27fc9682
gbubemi22 <slimnfine22@gmail.com>
2023-06-19 19:15:35 +0100

    auth/roles

---
+++ middleware/authorization.go
@@ -0,0 +1,41 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gbubemi22/go-rentals/models"
+	"net/http"
+)
+
+func Authorize(roles ...string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		userType, exists := c.Get("user_type")
+		if !exists {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "User not authenticated"})
+			c.Abort()
+			return
+		}
+
+		userTypeStr, ok := userType.(string)
+		if !ok {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid user type"})
+			c.Abort()
+			return
+		}
+
+		authorized := false
+		for _, allowedRole := range roles {
+			if allowedRole == userTypeStr {
+				authorized = true
+				break
+			}
+		}
+
+		if !authorized {
+			c.JSON(http.StatusForbidden, gin.H{"error": "Unauthorized"})
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
